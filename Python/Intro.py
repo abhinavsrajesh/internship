3d1edfb04fcb3501fe9e9c4e73a1226a96367bfe
abhinavsrajesh <132331288+abhinavsrajesh@users.noreply.github.com>
2023-05-08 11:41:03 +0530

    Update Intro.py

--- Python/Intro.py
+++ Python/Intro.py
@@ -2,5 +2,5 @@ name="Abhinav S Rajesh"
 age=18
 clg="GPTC Chelakkara, Thrissur"
 dept="Computer Engineering"
-place="palakkad"
-print("Self intro\n""My name is", name,". \nI'm",age, ". \nI'm coming from", place, ". \nI'm Studying at", clg,"\nas a", dept, "Student .")
+place="Palakkad"
+print("\tSelf intro\n" "\t-----------\n" "My name is", name,". \nI'm",age, ". \nI'm coming from", place, ". \nI'm Studying at", clg,"\nas a", dept, "Student .")
