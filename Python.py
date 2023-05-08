1030f7a947579ff9cd8615775ea01f7465f50726
abhinavsrajesh <132331288+abhinavsrajesh@users.noreply.github.com>
2023-05-08 11:22:20 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,6 @@
+name="Abhinav S Rajesh"
+age=18
+clg="GPTC Chelakkara, Thrissur"
+dept="Computer Engineering"
+place="palakkad"
+print("Self intro\n""My name is", name,". \nI'm",age, ". \nI'm coming from", place, ". \nI'm Studying at", clg,"\nas a", dept, "Student .")
