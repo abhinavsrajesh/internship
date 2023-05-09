84a0139bd68772e4a43277ca070f088289807070
abhinavsrajesh <132331288+abhinavsrajesh@users.noreply.github.com>
2023-05-09 10:02:41 +0530

    Create odd&even.py

---
+++ Python/odd&even.py
@@ -0,0 +1,8 @@
+print("1 to 10 odd numbers")
+print("-------------------")
+for i in range(1,11,2): #loop condition for odd numbers
+    print(i)
+print("1 to 10 even numbers")
+print("-------------------")
+for i in range(0,11,2): #loop condition for even numbers
+    print(i)
